68054b8088b3f018b1f4b58ad20e87e38d730ff8
Usman <u2003252@gmail.com>
2025-04-04 01:26:30 +0500

    Implemented user registration view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+# from rest_framework import status
+# from rest_framework.response import Response
+# from rest_framework.decorators import api_view
+from .serializers import UserSerializer
+from django.contrib.auth.models import User
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework import generics
+from rest_framework import serializers
 
-# Create your views here.
+
+class CreateUserView(generics.CreateAPIView): 
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+    def perform_create(self, serializer):
+        if User.objects.filter(email=self.request.data.get('email')).exists():
+            raise serializers.ValidationError({"email": "Email already registered."})
+        serializer.save()
